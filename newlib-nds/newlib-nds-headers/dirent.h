28f1fb69bdbb27ee737647cd587df587a548e56d
Coto <programmer.coto.1988@gmail.com>
2019-08-03 14:54:58 -0400

    rename fsfat to fatfs

--- newlib-nds/newlib-nds-headers/dirent.h
+++ newlib-nds/newlib-nds-headers/dirent.h
@@ -7,7 +7,7 @@
 
 #include <sys/dirent.h>
 
-//this dirent is guaranteed to work with fsfat, add items if you want but don't edit those definitions.
+//this dirent is guaranteed to work with fatfs, add items if you want but don't edit those definitions.
 struct dirent {
 	ino_t d_ino;				/* file serial number */ //used as struct fd index ( fd_struct_get(fd) ), assigned when open newlib posix call, and freed when close newlib posix call
     char d_name[NAME_MAX+1];	/* name must be no longer than this */
