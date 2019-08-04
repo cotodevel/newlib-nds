d5e079d87b05fe6b74e1c1d2657cb42fde227795
Coto <programmer.coto.1988@gmail.com>
2019-08-04 16:47:06 -0400

    use standard names for some TGDS FS functions.

--- newlib-nds/newlib-nds-headers/dirent.h
+++ newlib-nds/newlib-nds-headers/dirent.h
@@ -9,7 +9,7 @@
 
 //this dirent is guaranteed to work with fatfs, add items if you want but don't edit those definitions.
 struct dirent {
-	ino_t d_ino;				/* file serial number */ //used as struct fd index ( fd_struct_get(fd) ), assigned when open newlib posix call, and freed when close newlib posix call
+	ino_t d_ino;				/* file serial number -- Index == Posix d_ino "Serial Number" file handle = getStructFD(index) */
     char d_name[NAME_MAX+1];	/* name must be no longer than this */
 };
 
