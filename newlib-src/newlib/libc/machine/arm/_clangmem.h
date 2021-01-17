19a4c3e6a056393aedf6b191b5062b24aa2eda17
Coto <programmer.coto.1988@gmail.com>
2021-01-17 20:08:22 -0300

    moving ARM9 core onto Clang compiler.
    Clang can't build .C code from libcnano9 or causes segfaults. Todo, fix this.
    
    TGDS-template Binary working

--- newlib-src/newlib/libc/machine/arm/_clangmem.h
+++ newlib-src/newlib/libc/machine/arm/_clangmem.h
@@ -3,6 +3,7 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <typedefsTGDS.h>
 
 #ifdef __cplusplus
 extern "C"{
@@ -14,6 +15,8 @@ extern void __aeabi_memset8(void *dest, size_t n, int c);
 
 extern void __aeabi_memclr(void *dest, size_t n);
 extern void __aeabi_memcpy(void *dest, void *src, size_t n);
+extern void __aeabi_memmove (void *dest, const void *src, size_t n);
+extern uint64_t __aeabi_lmul(uint32_t a, uint32_t b);
 
 #ifdef __cplusplus
 }
--- newlib-src/newlib/libc/machine/arm/_clangmemC.c
+++ newlib-src/newlib/libc/machine/arm/_clangmemC.c
@@ -3,3 +3,22 @@
 
 #include "_clangmem.h"
 
+void __aeabi_memmove (void *dest, const void *src, size_t n){
+	memmove (dest, src, n);
+}
+
+uint64_t __aeabi_lmul(uint32_t a, uint32_t b){
+    uint32_t hia = a >> 16,
+        hib = b >> 16,
+        loa = (uint32_t)(uint16_t)a,
+        lob = (uint32_t)(uint16_t)b,
+        low = loa * lob,
+        mid1 = hia * lob,
+        mid2 = loa * hib,
+        mid = mid1 + mid2,
+        high = hia * hib;
+
+    if (mid < mid1)
+        high += 0x10000;
+    return ((uint64_t)high << 32) + ((uint64_t)mid << 16) + low;
+}
